4aedc5ebfe14aacbd2235b46767e2cf5d074a454
Clement-Wright <clewri163@gmail.com>
2023-12-05 18:42:08 -0500

    aaaaa

--- src/index.js
+++ src/index.js
@@ -1,30 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "@aws-amplify/ui-react/styles.css";
 import { Amplify } from 'aws-amplify';
-import { ThemeProvider } from '@aws-amplify/ui-react';
-import awsconfig from './aws-exports';
-import {EquationNoteCardCollection, EquationNoteCard, EquationNavBar} from "./ui-components";
-import { BrowserRouter } from 'react-router-dom';
-import {studioTheme} from './ui-components';
-Amplify.configure(awsconfig);
+import config from './aws-exports';
+Amplify.configure(config);
 
 
 
 const container = document.getElementById("root");
 
-const root = createRoot(container);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={studioTheme}>
-        <App/>
-      </ThemeProvider>
-    </BrowserRouter>
+    <App/>
   </React.StrictMode>
 );
 
@@ -32,3 +23,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+

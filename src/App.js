2c5ee50eead75b9c849d9a5186b0c0c7e1ca74d3
Clement-Wright <clewri163@gmail.com>
2023-12-05 19:15:41 -0500

    pls work plsssssssss

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-
 import React, { Component } from 'react'
 import './App.css';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import {EquationNoteCardCollection, EquationNewNote, EquationNavBar, EquationCreateForm, EquationUpdateForm} from "./ui-components";
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 class App extends Component {
     render() {
     return (
-        <div className="App"><header className="App-header">
+        <div className="App"><header className="App-header">\
             <Routes>
                 <Route exact path='/' element={<div><EquationNavBar/><EquationNoteCardCollection isPaginated itemsPerPage={3} /></div>}/>
                 <Route exact path='/newnote' element={<EquationCreateForm/>}/>
-                <Route exact path='/create' element={<EquationNewNote/>}/>
-                <Route exact path='/update' element={<EquationUpdateForm/>}/>
+                <Route exact path='/edit' element={<EquationUpdateForm/>}/>
             </Routes>
-        </header></div>
+            </header></div>
     )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "@aws-amplify/ui-react/styles.css";
 import { Amplify } from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
+import { ThemeProvider } from '@aws-amplify/ui-react';
+import awsconfig from './aws-exports';
+import {EquationNoteCardCollection, EquationNoteCard, NewNavBar} from "./ui-components";
+import { BrowserRouter } from 'react-router-dom';
+import {studioTheme} from './ui-components';
+Amplify.configure(awsconfig);
 
 
 
 const container = document.getElementById("root");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(container);
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={studioTheme}>
+        <App/>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-

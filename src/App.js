e93a1f16321c275627bdf6581adb4b76717ebf14
Clement-Wright <clewri163@gmail.com>
2023-12-05 19:00:40 -0500

    aaaaaaaaaawwwwwwww

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import React, { Component } from 'react'
 import './App.css';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import {EquationNoteCardCollection, EquationNewNote, EquationNavBar, EquationCreateForm, EquationUpdateForm} from "./ui-components";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 
 class App extends Component {
     render() {
     return (
         <div className="App"><header className="App-header">
-          <Router>
             <Routes>
                 <Route exact path='/' element={<div><EquationNavBar/><EquationNoteCardCollection isPaginated itemsPerPage={3} /></div>}/>
                 <Route exact path='/newnote' element={<EquationCreateForm/>}/>
                 <Route exact path='/create' element={<EquationNewNote/>}/>
                 <Route exact path='/update' element={<EquationUpdateForm/>}/>
             </Routes>
-          </Router>
-            </header></div>
+        </header></div>
     )
     }
 }
